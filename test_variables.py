7754b8f56222ac8f4321dc6aff9e79105c7247ee
Drew Michael <dmichael@fastly.com>
2026-08-31 17:46:49 -0600

    chore: clean up stray test files

--- test_variables.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-cw = boto3.client('cloudwatch')
-paginator = cw.get_paginator('list_metrics')
-for response in paginator.paginate(Namespace='Fastly/RealTime'):
-    print(response['Metrics'])
-    break
